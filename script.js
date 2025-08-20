84177a917aa05f2bca7b1872ab68ddf4f030cac8
Eberson Can <ecan@tecniscan.com>
2025-08-20 12:19:56 -0600

    test

--- script.js
+++ script.js
@@ -9,13 +9,51 @@ _dbPromise = (async () => {
 const SQL = await initSqlJs({
 locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.10.2/${file}`
 });
-const resp = await fetch('../DB/Proyecto1.db');
-if (!resp.ok) throw new Error('No se pudo cargar residencial.db');
+const resp = await fetch('https://ecans-0860.github.io/DB/Proyecto1.db');
+if(resp.ok) console.log('DB Cargada Correctamente')
+if (!resp.ok) throw new Error('No se pudo cargar la base de datos');
 const buf = await resp.arrayBuffer();
 const db = new SQL.Database(new Uint8Array(buf));
+console.log(db)
+
 return db;
 })();
 return _dbPromise;
+}
+
+async function showTable() {
+  const db = await initDb();
+      const tables = db.exec("SELECT name FROM sqlite_master WHERE type='table';");
+   
+    if (tables.length > 0) {
+      const tableNames = tables[0].values.map(row => row[0]);
+      console.log("Nombres de tablas:", tableNames);
+    }
+
+  // Ejecutamos la consulta"
+
+  const res = db.exec(`SELECT COUNT(*) as total FROM "Calendario";`);
+console.log(res);
+
+  if (res.length === 0) {
+    console.log("⚠️ La tabla está vacía o no existe");
+    return;
+  }
+
+  // Extraer columnas y valores
+  const columns = res[0].columns;
+  const values = res[0].values;
+
+  // Construir tabla HTML
+  let html = "<table border='1' cellspacing='0' cellpadding='5'>";
+  html += "<tr>" + columns.map(col => `<th>${col}</th>`).join("") + "</tr>";
+  values.forEach(row => {
+    html += "<tr>" + row.map(val => `<td>${val}</td>`).join("") + "</tr>";
+  });
+  html += "</table>";
+
+  // Insertar en el HTML
+  document.getElementById("output").innerHTML = html;
 }
 
 
